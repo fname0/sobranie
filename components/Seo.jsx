df93798ef4ffcafcfb2d044146beea4d9f2bbdc3
fname0 <kmailfw@mail.ru>
2023-07-17 18:51:53 +0300

    с

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -5,14 +5,14 @@ const Seo = ({title, keywords, description}) => {
     <Head>
         <title>{title}</title>
         <meta property="og:title" content={title} />
-        {/* <meta property="og:image" content="/icon.ico" /> */}
+        <meta property="og:image" content="/icon.ico" />
         <meta name='description' content={description}/>
         <meta property="og:description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="keywords" content={keywords} />
         <meta charSet="utf-8"></meta>
-        {/* <link rel="icon" href="https://sobranie.vercel.app/icon.ico" type="image/x-icon"/> */}
-        {/* <link rel="apple-touch-icon" href="/icon.ico"/> */}
+        <link rel="icon" href="https://sobranie.vercel.app/icon.ico" type="image/x-icon"/>
+        <link rel="apple-touch-icon" href="/icon.ico"/>
     </Head>
      );
 }
